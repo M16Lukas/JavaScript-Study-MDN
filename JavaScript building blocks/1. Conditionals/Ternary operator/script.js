3d50f07431f84e94511aa3036ed5b600dfc33898
Park <46748131+M16Lukas@users.noreply.github.com>
2021-02-16 21:12:54 +0900

    Create script.js

---
+++ JavaScript building blocks/1. Conditionals/Ternary operator/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const select = document.querySelector('select');
+const html = document.querySelector('html');
+
+document.body.style.padding = '10px';
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.onchange = () => {
+  (select.value === 'black') ? update('black', 'white') : update('white', 'black');
+}
